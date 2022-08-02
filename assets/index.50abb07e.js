3fc38fee07f1eb40e30c6e82d43011710321a6fc
Sammy Guttman <1449@holbertonschool.com>
2022-08-02 13:27:17 -0500

    Updates

---
+++ assets/index.50abb07e.js
@@ -0,0 +1,3 @@
+import{s as a,j as e}from"./index.150bbc86.js";import t from"./index.618b8407.js";const r=a.header.attrs({className:"StyledHeader bg-ilusion-header"})`
+  ${{height:"3.5rem",width:"100%",paddingTop:"0.75rem",paddingBottom:"0.75rem",paddingLeft:"1.5rem",paddingRight:"1.5rem"}}
+`,i=()=>e(r,{children:e(t,{variant:"h2",className:"text-white",children:"Status Dashboard"})});export{i as default};
