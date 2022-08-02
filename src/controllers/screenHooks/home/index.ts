35c8b5a14e2fa3995d6c5890ba3608b8939c08a7
Sammy Guttman <1449@holbertonschool.com>
2022-08-02 11:24:25 -0500

    feat: add node version to deploy

---
+++ src/controllers/screenHooks/home/index.ts
@@ -0,0 +1,35 @@
+// Package
+import {useEffect} from "react"
+
+// Actions
+import useActions from "@/api/actions";
+import useModels from "@/models";
+
+const useHome = () => {
+  const listName = ["accounts", "assets", "customers", "datapoints", "devices", "documents", "forms", "invites",
+    "media", "messages", "namespaces", "orders", "patients", "relationships", "rules",
+    "templates", "users", "workflows"]
+
+  // Action
+  const { dispatch, useGeneralActions } = useActions();
+  const { actGetStatus } = useGeneralActions();
+
+  const { useSelectors } = useModels();
+  const { useSelector, useGeneralSelectors } = useSelectors();
+  const { generalSettings } = useGeneralSelectors();
+  const { list } = useSelector(generalSettings);
+
+
+  useEffect(() => {
+    setInterval(()=>
+    listName.forEach((name) => {
+      dispatch(actGetStatus(name));
+    }), import.meta.env.VITE_REFRESH_TIME * 1000)
+  }, []);
+
+  return {
+    list,
+  };
+};
+
+export default useHome;
